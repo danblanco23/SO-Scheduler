279094d98bc7dfdecc452db67e339b522d06bf69
dereck5 <dereck.benavides@schoolmint.com>
2020-06-02 19:59:26 -0600

    new files

---
+++ .idea/Calendarizer.h
@@ -0,0 +1,16 @@
+//
+// Created by Dereck Escalante on 5/30/20.
+//
+
+#ifndef TP2_OPERATIVOS_CALENDARIZER_H
+#define TP2_OPERATIVOS_CALENDARIZER_H
+struct Calendarizer {
+    int algorithm;
+    int operationMode;
+    int numberProcess;
+    int quantum;
+};
+typedef struct Calendarizer Calendar ;
+void FCFS(Calendar);
+
+#endif //TP2_OPERATIVOS_CALENDARIZER_H
---
+++ .idea/Process.h
@@ -0,0 +1,19 @@
+//
+// Created by Dereck Escalante on 5/30/20.
+//
+
+#ifndef TP2_OPERATIVOS_PROCESS_H
+#define TP2_OPERATIVOS_PROCESS_H
+struct Process{
+    int processNumber;
+    int jobDone;
+    int iteration;
+    long double lastTaylor;
+    long double result;
+    int processArrival;
+    int totalJob;
+
+};
+typedef struct Process Process ;
+
+#endif //TP2_OPERATIVOS_PROCESS_H
